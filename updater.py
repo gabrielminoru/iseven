b5de8b6f26b6584524cdffc3acdae5b18d8692ed
Gabriel <minoru@minoru.local>
2026-01-08 13:26:47 -0300

    first commit

---
+++ updater.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+from main import is_even
+
+main_file_path = Path("main.py")
+main_file_content = main_file_path.read_text()
+
+numbers = [int(i) for i in main_file_content.split(" ") if i.isdigit()]
+largest = max(numbers)
+new = largest + 1
+new_retval = not is_even(largest)
+new_retval_str = str(new_retval).capitalize()
+
+aux_replace = """        case _:"""
+
+new_case = f"""case {new} | -{new}:
+            return {new_retval_str}""" + "\n" + aux_replace
+
+main_file_path.write_text(main_file_content.replace("case _:", new_case))
+print(f"Added case {new} | -{new} with return value of {new_retval_str}")
